16c44a7d2ba12dc2e91c72fa013509ecd5bf33c1
JoelKanyi <joelkanyi98@gmail.com>
2021-05-10 12:25:24 +0300

    screwed up the viewmodels

--- app/src/main/java/com/kanyideveloper/rickymortyapi/MainActivity.kt
+++ app/src/main/java/com/kanyideveloper/rickymortyapi/MainActivity.kt
@@ -2,11 +2,15 @@ package com.kanyideveloper.rickymortyapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Adapter
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import com.kanyideveloper.rickymortyapi.adapter.RecyclerAdapter
 import com.kanyideveloper.rickymortyapi.databinding.ActivityMainBinding
+import com.kanyideveloper.rickymortyapi.network.ResultData
 import com.kanyideveloper.rickymortyapi.viewmodel.MainActivityViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
@@ -16,6 +20,37 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var recyclerAdapter: RecyclerAdapter
+    private val mainViewModel : MainActivityViewModel by viewModels()
+    private val TAG = "MainActivity"
+
+
+
+    private val repositoryObserver = Observer<ResultData<Any>> { resultData ->
+        when (resultData) {
+            is ResultData.Loading -> {
+                //binding.progressIndicator.show()
+
+                Log.d(TAG, "Loading ")
+            }
+            is ResultData.Success -> {
+                //binding.progressIndicator.hide()
+                resultData.data
+                //adapter.submitList(resultData.data)
+
+                Log.d(TAG, "Success: ${resultData.data?.toString()} ")
+            }
+            is ResultData.Failed -> {
+                //binding.progressIndicator.hide()
+
+                Log.d(TAG, "Failed: ")
+            }
+            is ResultData.Exception -> {
+                //binding.progressIndicator.hide()
+
+                Log.d(TAG, "Exception Caught: ${resultData.exception}")
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,8 +59,7 @@ class MainActivity : AppCompatActivity() {
 
         recyclerAdapter = RecyclerAdapter()
 
-        initRecyclerView()
-        initViewModel()
+        getDataAndSubscribeEvents()
     }
 
     private fun initRecyclerView(){
@@ -34,13 +68,18 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun initViewModel(){
+    /*private fun initViewModel(){
         val viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
         lifecycleScope.launchWhenCreated {
             viewModel.getListData().collectLatest {
                 recyclerAdapter.submitData(it)
             }
         }
+    }*/
+
+    private fun getDataAndSubscribeEvents() {
+        val repositoriesListLiveData = mainViewModel.getListData()
+        repositoriesListLiveData.observe(this, repositoryObserver)
     }
 }
 
