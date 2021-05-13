5f3633c02169e83c92fde99c69c38913ab1f6ed4
JoelKanyi <joelkanyi98@gmail.com>
2021-05-13 11:48:54 +0300

    added loading state

--- app/src/main/java/com/kanyideveloper/rickymortyapi/ui/MainActivity.kt
+++ app/src/main/java/com/kanyideveloper/rickymortyapi/ui/MainActivity.kt
@@ -3,12 +3,16 @@ package com.kanyideveloper.rickymortyapi.ui
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
+import androidx.paging.LoadState
 import com.kanyideveloper.rickymortyapi.adapter.RecyclerAdapter
 import com.kanyideveloper.rickymortyapi.databinding.ActivityMainBinding
 import com.kanyideveloper.rickymortyapi.viewmodel.MainActivityViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -17,6 +21,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var recyclerAdapter: RecyclerAdapter
     private val mainViewModel: MainActivityViewModel by viewModels()
 
+    private var searchJob: Job? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -24,20 +30,55 @@ class MainActivity : AppCompatActivity() {
 
         recyclerAdapter = RecyclerAdapter()
 
-        initRecyclerView()
-        initViewModel()
+        setUpAdapter()
+        startSearchJob()
     }
 
-    private fun initRecyclerView() {
-        binding.recyclerview.apply {
-            adapter = recyclerAdapter
+
+    private fun startSearchJob() {
+
+        searchJob?.cancel()
+        searchJob = lifecycleScope.launch {
+            mainViewModel.getListData()
+                .collectLatest {
+                    recyclerAdapter.submitData(it)
+                }
         }
     }
 
-    private fun initViewModel() {
-        lifecycleScope.launchWhenCreated {
-            mainViewModel.getListData().collectLatest {
-                recyclerAdapter.submitData(it)
+
+    private fun setUpAdapter() {
+
+        binding.recyclerview.apply {
+            adapter = recyclerAdapter
+        }
+
+        recyclerAdapter.addLoadStateListener { loadState ->
+
+            if (loadState.refresh is LoadState.Loading) {
+
+                if (recyclerAdapter.snapshot().isEmpty()) {
+                    binding.progressIndicator.isVisible = true
+                }
+                binding.errorTextView.isVisible = false
+
+            } else {
+                binding.progressIndicator.isVisible = false
+
+                val error = when {
+                    loadState.prepend is LoadState.Error -> loadState.prepend as LoadState.Error
+                    loadState.append is LoadState.Error -> loadState.append as LoadState.Error
+                    loadState.refresh is LoadState.Error -> loadState.refresh as LoadState.Error
+
+                    else -> null
+                }
+                error?.let {
+                    if (recyclerAdapter.snapshot().isEmpty()) {
+                        binding.errorTextView.isVisible = true
+                        binding.errorTextView.text = it.error.localizedMessage
+                    }
+                }
+
             }
         }
     }
